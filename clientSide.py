e995c83e93d313926438ab3458382cbf89783562
RaniBezalel <ranib654321@gmail.com>
2023-12-26 18:40:37 +0200

    RunningVersion

---
+++ clientSide.py
@@ -0,0 +1,36 @@
+import socket, select, sys
+
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 8080
+
+def runClient():
+    try:
+        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        clientSocket.connect((SERVER_IP, SERVER_PORT))
+
+    except socket.error as e:
+        print(f"Socket error: {e}")
+        sys.exit()
+
+    connectionUsername = input("Enter your name: ")
+    clientSocket.send(connectionUsername.encode("utf-8"))
+    print(f"Hi {connectionUsername}, if you wish to leave write exit")
+    print("-------------------------------------")
+
+    while True:
+        currentSockets, _, _ = select.select([clientSocket, sys.stdin], [], [])
+
+        for sock in currentSockets:
+            if sock == sys.stdin:
+                message = sys.stdin.readline().strip()
+                clientSocket.send(message.encode("utf-8"))
+
+                if(message.lower() == "exit"):
+                    clientSocket.close()
+                    sys.exit()
+        else:
+            serverResponse = (clientSocket.recv(1024)).decode("utf-8")
+            print(serverResponse)
+       
+if __name__ == '__main__':
+     runClient()
---
+++ serverSide.py
@@ -0,0 +1,54 @@
+import socket, threading
+
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 8080
+currentClients = []
+
+def serverInit():
+    try:
+        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serverSocket.bind((SERVER_IP, SERVER_PORT))
+        serverSocket.listen(0)
+        print("Server is up and running")
+        
+    except socket.error as e:
+        print(f"Socket error: {e}")
+
+    while True:
+        clientSocket, _ = serverSocket.accept()
+        currentClients.append(clientSocket)
+
+        try:
+            newThread = threading.Thread(target=manageClient, args=(clientSocket, ))
+            newThread.start()
+
+        except threading.ThreadError as e:
+            print(f"Thread error: {e}")
+
+
+def sendMessageToAllClients(response):
+        for client in currentClients:
+            try:
+                client.send(response.encode("utf-8"))
+            except:
+                 currentClients.remove(client)
+
+def manageClient(clientSocket):
+        clientUsername = (clientSocket.recv(1024)).decode("utf-8")
+        welcomeMessage = f"{clientUsername} has joined the chatroom"
+        sendMessageToAllClients(welcomeMessage)
+
+        # receive data from the client
+        while True:
+            message = (clientSocket.recv(1024).decode("utf-8")).strip()
+            response = f"({clientUsername}):{message}"
+
+            if (message.lower() == "exit"):
+                sendMessageToAllClients(f"{clientUsername} has now left the chatroom")
+                currentClients.remove(clientSocket)
+                break
+            
+            sendMessageToAllClients(response)
+
+if __name__ == '__main__':
+     serverInit()
